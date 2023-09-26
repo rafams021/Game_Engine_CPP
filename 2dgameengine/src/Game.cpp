68fa4aa7e7f8961b4e66a8a1b76ce632c303d885
rafams021 <rafaelms.021@gmail.com>
2023-09-26 15:38:07 -0400

    imeplement movement and delta time

--- 2dgameengine/src/Game.cpp
+++ 2dgameengine/src/Game.cpp
@@ -1,48 +1,45 @@
 #include "Game.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
-Game::Game(/* args */) {
-    std::cout << " Game Constructor called!!" << std::endl;
+Game::Game() {
     isRunning = false;
+    std::cout << "Game constructor called!" << std::endl;    
 }
 
 Game::~Game() {
-    std::cout << " Game Destructor called!!" << std::endl;
+    std::cout << "Game destructor called!" << std::endl;    
 }
 
 void Game::Initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cerr << "Error initializing SDL" << std::endl;
+        std::cerr << "Error initializing SDL." << std::endl;
         return;
     }
-
     SDL_DisplayMode displayMode;
     SDL_GetCurrentDisplayMode(0, &displayMode);
-
-    windowWidth = 800;
-    windowHeight = 600;
-
+    windowWidth = displayMode.w;
+    windowHeight = displayMode.h;
     window = SDL_CreateWindow(
-                            NULL, 
-                            SDL_WINDOWPOS_CENTERED, 
-                            SDL_WINDOWPOS_CENTERED, 
-                            windowWidth,
-                            windowHeight,
-                            SDL_WINDOW_BORDERLESS);
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        windowWidth,
+        windowHeight,
+        SDL_WINDOW_BORDERLESS
+    );
     if (!window) {
-        std::cerr << "Error creating SDL window." <<std::endl;
+        std::cerr << "Error creating SDL window." << std::endl;
         return;
     }
-    
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
         std::cerr << "Error creating SDL renderer." << std::endl;
         return;
     }
     SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
     isRunning = true;
 }
 
@@ -54,30 +51,55 @@ void Game::ProcessInput() {
                 isRunning = false;
                 break;
             case SDL_KEYDOWN:
-                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) isRunning = false;
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    isRunning = false;
+                }
                 break;
         }
     }
 }
 
-void Game::Setup() {
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
 
+void Game::Setup() {
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(100.0, 0.0);
 }
 
 void Game::Update() {
+    // If we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+    if(timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
 
+    // The diference in ticks since last frame, converted to seconds.
+    double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+
+    // Store the "previous" frame time
+    millisecsPreviousFrame = SDL_GetTicks();
+
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
 
+    // Loads a PNG texture
     SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
-    
-    // Destination rectangle to place the texture
-    SDL_Rect dstRect = {10, 10, 32, 32};
+
+    // What is the destination rectangle that we want to place our texture
+    SDL_Rect dstRect = {
+        static_cast<int>(playerPosition.x), 
+        static_cast<int>(playerPosition.y), 
+        32,
+        32
+    };
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
     SDL_DestroyTexture(texture);
 
--- 2dgameengine/src/Game.h
+++ 2dgameengine/src/Game.h
@@ -1,25 +1,31 @@
 #ifndef GAME_H
 #define GAME_H
+
 #include <SDL2/SDL.h>
 
-class Game {
-private:
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    bool isRunning;
-public:
-    Game(/* args */);
-    ~Game();
-    void Initialize();
-    void Run();
-    void ProcessInput();
-    void Update();
-    void Render();
-    void Destroy();
-    void Setup();
+const int FPS = 60;
+const int MILLISECS_PER_FRAME = 1000 / FPS;
 
-    int windowWidth;
-    int windowHeight;
+class Game {
+    private:
+        bool isRunning;
+        int millisecsPreviousFrame = 0;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+
+    public:
+        Game();
+        ~Game();
+        void Initialize();
+        void Run();
+        void Setup();
+        void ProcessInput();
+        void Update();
+        void Render();
+        void Destroy();
+
+        int windowWidth;
+        int windowHeight;
 };
 
 #endif
