542e8a144589ad284b42efa26694cdbc6b1e0e68
rafams021 <rafaelms.021@gmail.com>
2023-09-27 15:32:06 -0400

    ECS refactor implementation first

---
+++ 2dgameengine/src/Systems/MovementSystem.h
@@ -0,0 +1,23 @@
+#ifndef MOVEMENTSYSTEM_H
+#define MOVEMENTSYSTEM_H
+
+#include "ECS/ECS.h"
+
+class MovementSystem: public System {
+    public:
+        MovementSystem() {
+            // TODO:
+            // RequireComponent<TransformComponent>();
+            // RequireComponent<...>();
+        }
+
+        void Update() {
+            // TODO:
+            // Loop all entities that the system is interested in
+            // for (auto entity: GetEntities()) {
+                // TODO: Update entity position based on its velocity
+            // }
+        }
+};
+
+#endif
